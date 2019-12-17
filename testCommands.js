23f96df986657238373ee631a36e1de9c922b796
Meep3278 <zevgoldhabergordon@gmail.com>
2019-12-17 13:48:52 -0800

    Try basic on speak beQuietPlease

--- testCommands.js
+++ testCommands.js
@@ -1,14 +1,45 @@
-const Discord = require('discord.js');
- const client = new Discord.Client();
+const Discord = require("discord.js");
+const fs = require('fs');
 
-client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+const client = new Discord.Client();
+
+const config = require('./auth.json');
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+  if (msg.content.startsWith(config.prefix+'sicc')) {
+    let [command, ...channelName] = msg.content.split(" ");
+    if (!msg.guild) {
+      return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
+    }
+    const voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
+    //console.log(voiceChannel.id);
+    if (!voiceChannel || voiceChannel.type !== 'voice') {
+      return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
+    }
+    var beQuietFiles = ['./BeQuietPlease1.mp3', './BeQuietPlease2.mp3', './BeQuietPlease3.mp3', './BeQuietPlease1.mp3'];      
+    voiceChannel.join()
+      .then(conn => {
+        msg.reply('ready!');
+        // create our voice receiver
+        const receiver = conn.createReceiver();
 
-client.login('token');
+        conn.on('speaking', (user, speaking) => {
+          if (speaking) {
+            const dispatcher = connection.playFile(beQuietFiles[Math.floor(Math.random()*4)]);
+          }
+        });
+      })
+      .catch(console.log);
+  }
+  if(msg.content.startsWith(config.prefix+'leave')) {
+    let [command, ...channelName] = msg.content.split(" ");
+    let voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
+    voiceChannel.leave();
+  }
+});
+
+client.login("token");
+
+client.on('ready', () => {
+  console.log('ready!');
+});
